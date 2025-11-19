c55f8d7d8f1ad2776bc38b5868219ee08605c994
Ankush Kumar Gupta <167736782+ankush-coder-497001@users.noreply.github.com>
2025-11-19 10:27:20 +0530

    changes the flow

--- src/controllers/user.ctrl.js
+++ src/controllers/user.ctrl.js
@@ -35,13 +35,19 @@ const UserController = {
         return res.status(400).json({ message: "Error creating user" });
       }
       await newUser.save();
+
+      const token = await newUser.generateToken();
+      if (!token) {
+        return res.status(400).json({ message: "Error generating token" });
+      }
+
       EmailService.WelcomeEmail(
         email,
         name,
       ).catch((error) => {
         console.error('Error sending welcome email:', error);
       });
-      res.status(201).json({ message: "User registered successfully", user: newUser });
+      res.status(201).json({ message: "User registered successfully", user: newUser, token });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: error.message });
